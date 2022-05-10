8e85f439f0e5bce1bc0e44e0f9ff58326847215b
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2022-05-09 22:38:43 -0300

    Refactoring
    Refactoring whats work correctly

--- app_proverbs/lib/main.dart
+++ app_proverbs/lib/main.dart
@@ -54,7 +54,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _printMyText() async{
     Reader reader = Reader();
-    reader.readText();
     List<String> myTexts = await reader.getRandomTexts(5);
     print(myTexts);
   }
--- app_proverbs/lib/reader.dart
+++ app_proverbs/lib/reader.dart
@@ -7,42 +7,14 @@ import 'package:flutter/services.dart' show rootBundle;
 class Reader {
   List<String> myTexts = [];
 
-  // getText() async{
-  //   // List<String> contents = File('./rsc/proverbs_pt.txt').readAsLinesSync();
-  //   // var contents = getData();
-  //   // print("Look the contents:");
-  //   // print(contents);
-  // }
-  readText() async {
-    String response;
-    response = await rootBundle.loadString('rsc/proverbs_pt.txt');
-    // print(response);
-    LineSplitter ls = LineSplitter();
-    List<String> splitted = response.split('\n');
-    // List<String> splitted = ls.convert(response);
-    // Random r = Random();
-    // print(r.nextInt(2));
-    // print(splitted.length);
-    // print(splitted.elementAt(r.nextInt(splitted.length)));
-    _setMyTexts(splitted);
-    return await splitted.elementAt(0);
-  }
-
-  void _setMyTexts(List<String> value){
-    myTexts = value;
-  }
-
   getRandomTexts(int number) async{
     List<String> selected = [];
-    String response;
-    response = await rootBundle.loadString('rsc/proverbs_pt.txt');
-    LineSplitter ls = LineSplitter();
+    String response = await rootBundle.loadString('rsc/proverbs_pt.txt');
     List<String> splitted = response.split('\n');
     myTexts = splitted;
     Random r = Random();
     for (var i = 0; i < number; i++) {
       selected.add(myTexts.elementAt(r.nextInt(myTexts.length)));
-      print("printar $i vezes");
     }
     return selected;
   }
