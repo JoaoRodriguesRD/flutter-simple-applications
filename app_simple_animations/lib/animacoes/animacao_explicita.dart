ea5d5134048f831b09798e869d0e2a8e1bf27a83
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2022-06-16 16:28:05 -0300

    Issue correction

--- app_simple_animations/lib/animacoes/animacao_explicita.dart
+++ app_simple_animations/lib/animacoes/animacao_explicita.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class AnimacaoExplicita extends StatefulWidget {
   const AnimacaoExplicita({Key? key}) : super(key: key);
@@ -7,13 +8,45 @@ class AnimacaoExplicita extends StatefulWidget {
   State<AnimacaoExplicita> createState() => _AnimacaoExplicitaState();
 }
 
+// class _AnimacaoExplicitaState extends State<AnimacaoExplicita> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 300,
+//       height: 200,
+//       color: Colors.purple.shade500,
+//     );
+//   } 
+// }
 class _AnimacaoExplicitaState extends State<AnimacaoExplicita> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      height: 200,
-      color: Colors.purple.shade500,
-    );
+    return Row(
+  mainAxisSize: MainAxisSize.min,
+  children: <Widget>[
+    const SizedBox(width: 20.0, height: 100.0),
+    const Text(
+      'Be',
+      style: TextStyle(fontSize: 43.0),
+    ),
+    const SizedBox(width: 20.0, height: 100.0),
+    DefaultTextStyle(
+      style: const TextStyle(
+        fontSize: 40.0,
+        fontFamily: 'Horizon',
+      ),
+      child: AnimatedTextKit(
+        animatedTexts: [
+          RotateAnimatedText('AWESOME'),
+          RotateAnimatedText('OPTIMISTIC'),
+          RotateAnimatedText('DIFFERENT'),
+        ],
+        onTap: () {
+          print("Tap Event");
+        },
+      ),
+    ),
+  ],
+);
   }
 }
